acd72a311b87af4f4b556250c383e837863a367c
aboodijmail <aboodhih@yahoo.com>
2024-09-24 00:49:33 +0300

    inital commit

---
+++ test.js
@@ -0,0 +1,36 @@
+// THE DIFFRANCE BETWEEN (VAR,LET,CONST):
+//
+//    -THE SCOPE
+//
+//       WITH "VAR" WE CAN GET TO IT FROM ANYWERE
+//       REGARDLESS WHERE IT WAS (GLOBAL SCOPE OR LOCAL SCOPE).
+//
+//       VAR ISNT BLOCK SCOPED & FUNCTION SCOPE
+//       EVEN IF WE PUT IT INTO {} .
+//
+//       WITH "LET" WE CAN'T GET IT FROM ANYWERE
+//       BECAUSE ITS FUNCTION-SCOPED & BLOCK-SCOPED.
+//
+//       WITH "CONST" ALL TEARMMES OF "LET" APLYES ON IT EXEPT
+//       THAT WE USE IT ONLY TO DECLAR A CONSTANT ONLY.
+//
+
+
+
+function cumulative(array){
+    let total =array.reduce(function(prev,current){
+
+        return prev+current
+    })
+    let arr = [total,array.length]
+    return arr
+
+}
+
+console.log(arr);
+
+
+
+
+
+
